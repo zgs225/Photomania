991111bbe0ea3f5fc92dcbb2606f21732ebd646f
zgs225 <zgs225@gmail.com>
2014-08-04 23:34:11 +0800

    Use map view instead of table view to display photos by photographer

--- Photomania/PhotosByPhotographerMapViewController.m
+++ Photomania/PhotosByPhotographerMapViewController.m
@@ -9,10 +9,12 @@
 #import "PhotosByPhotographerMapViewController.h"
 #import <MapKit/MapKit.h>
 #import "Photo.h"
+#import "ImageViewController.h"
 
 @interface PhotosByPhotographerMapViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) NSArray *photosByPhotographer; // of Photo
+@property (strong, nonatomic) ImageViewController *imageViewController; // can be nil
 @end
 
 @implementation PhotosByPhotographerMapViewController
@@ -51,5 +53,121 @@
     [self.mapView addAnnotations:self.photosByPhotographer];
     [self.mapView showAnnotations:self.photosByPhotographer animated:YES];
 }
+
+- (ImageViewController *)imageViewController
+{
+    id detailvc = [self.splitViewController.viewControllers lastObject];
+    if ([detailvc isKindOfClass:[UINavigationController class]]) {
+        detailvc = [((UINavigationController *)detailvc).viewControllers firstObject];
+    }
+    return [detailvc isKindOfClass:[ImageViewController class]] ? detailvc : nil;
+}
+
+#pragma mark - Map view delegate
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    static NSString *reuseId = @"PhotosByPhotographerMapViewController";
+    MKAnnotationView *view = [self.mapView dequeueReusableAnnotationViewWithIdentifier:reuseId];
+    
+    if (!view) {
+        view = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:reuseId];
+        view.canShowCallout = YES;
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 46, 46)];
+        view.leftCalloutAccessoryView = imageView;
+        view.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    }
+    
+    view.annotation = annotation;
+    
+    return view;
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view
+{
+    [self updateLeftCalloutAccessoryViewInAnnotationView:view];
+}
+
+- (void)updateLeftCalloutAccessoryViewInAnnotationView:(MKAnnotationView *)annotationView
+{
+    UIImageView *imageView = nil;
+    if ([annotationView.leftCalloutAccessoryView isKindOfClass:[UIImageView class]]) {
+        imageView = (UIImageView *)annotationView.leftCalloutAccessoryView;
+    }
+    if (imageView) {
+        Photo *photo = nil;
+        if ([annotationView.annotation isKindOfClass:[Photo class]]) {
+            photo = (Photo *)annotationView.annotation;
+        }
+        if (photo) {
+            NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:photo.thumbnailURL]];
+            
+            // another configuration option is backgroundSessionConfiguration (multitasking API required though)
+            NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+            
+            // create the session without specifying a queue to run completion handler on (thus, not main queue)
+            // we also don't specify a delegate (since completion handler is all we need)
+            NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+            
+            NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request
+                                                            completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+                                                                // this handler is not executing on the main queue, so we can't do UI directly here
+                                                                if (!error) {
+                                                                    if ([request.URL isEqual:[NSURL URLWithString:photo.thumbnailURL]]) {
+                                                                        // UIImage is an exception to the "can't do UI here"
+                                                                        UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                                                                        // but calling "self.image =" is definitely not an exception to that!
+                                                                        // so we must dispatch this back to the main queue
+                                                                        dispatch_async(dispatch_get_main_queue(), ^{ imageView.image = image; });
+                                                                    }
+                                                                }
+                                                            }];
+            [task resume]; // don't forget that all NSURLSession tasks start out suspended!
+        }
+    }
+}
+
+#pragma mark - Navigation
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    if (self.imageViewController) {
+        [self prepareViewController:self.imageViewController
+                           forSegue:nil
+                   toShowAnnotation:view.annotation];
+    } else {
+        [self performSegueWithIdentifier:@"Show Photo" sender:view];
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([sender isKindOfClass:[MKAnnotationView class]]) {
+        [self prepareViewController:segue.destinationViewController
+                           forSegue:segue.identifier
+                   toShowAnnotation:((MKAnnotationView *)sender).annotation];
+    }
+}
+
+- (void)prepareViewController:(id)vc
+                     forSegue:(NSString *)segueIdentifier
+             toShowAnnotation:(id<MKAnnotation>)annotation
+{
+    Photo *photo = nil;
+    if ([annotation isKindOfClass:[Photo class]]) {
+        photo = (Photo *)annotation;
+    }
+    if (photo) {
+        if (![segueIdentifier length] || [segueIdentifier isEqualToString:@"Show Photo"]) {
+            if ([vc isKindOfClass:[ImageViewController class]]) {
+                ImageViewController *ivc = (ImageViewController *)vc;
+                ivc.imageURL = [NSURL URLWithString:photo.imageURL];
+                ivc.title = photo.title;
+            }
+        }
+    }
+}
+
+
 
 @end
